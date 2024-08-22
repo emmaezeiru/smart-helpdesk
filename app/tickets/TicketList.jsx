3a80aee145d6de922b7b280ec604b65b78045441
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2024-08-22 16:21:33 +0100

    linked ticket list page to ticket details page

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 async function getTickets() {
     const res = await fetch('http://localhost:8000/tickets', {
         next: {
@@ -14,11 +16,13 @@ export default async function TicketList() {
     <>
         {tickets.map((ticket)=> (
             <div className="card my-5" key={ticket.id}>
-                <h3>{ ticket.title }</h3>
-                <p>written by {ticket.body.slice(0, 200)}...</p>
-                <div className={`pill ${ticket.priority}`}>
-                    {ticket.priority} priority
-                </div>
+                <Link href={`/tickets/${ticket.id}`}>
+                    <h3>{ ticket.title }</h3>
+                    <p>written by {ticket.body.slice(0, 200)}...</p>
+                    <div className={`pill ${ticket.priority}`}>
+                        {ticket.priority} priority
+                    </div>
+                </Link>
             </div>
         ))}
         {tickets.length === 0 && (
