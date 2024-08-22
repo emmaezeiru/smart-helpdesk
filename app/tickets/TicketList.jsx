dc80cca77de08df13798bf4b84f246de893f4c7b
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2024-08-22 15:56:04 +0100

    function to fetch tickets in ticketlist

---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,29 @@
+async function getTickets() {
+    const res = await fetch('http://localhost:8000/tickets', {
+        next: {
+            revalidate: 0 // use 0 to opt out of using cache data
+        }
+    })
+
+    return res.json()
+}
+
+export default async function TicketList() {
+    const tickets = await getTickets()
+  return (
+    <>
+        {tickets.map((ticket)=> (
+            <div className="card my-5" key={ticket.id}>
+                <h3>{ ticket.title }</h3>
+                <p>written by {ticket.body.slice(0, 200)}...</p>
+                <div className={`pill ${ticket.priority}`}>
+                    {ticket.priority} priority
+                </div>
+            </div>
+        ))}
+        {tickets.length === 0 && (
+            <p className="text-center">There are no open tickets!</p>
+        )}
+    </>
+  )
+}
