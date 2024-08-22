59a9651321ea33d9770ad7719d5f3216fc597501
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2024-08-22 15:55:07 +0100

    created home, navbar and tickets page

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,11 +1,23 @@
+import Image from "next/image";
 import Link from "next/link";
+import Logo from './smart-logo.jpg'
 
 export default function Navbar() {
   return (
     <nav>
-        <h2>smart helpdesk</h2>
-        <Link href="/">Dashboard</Link>
-        <Link href="/tickets">Tickets</Link>
+      <Image
+        src={Logo}
+        alt='smart logo'
+        width={50}
+        quality={100}
+        placeholder="blur"
+      />
+      
+      <Link href="/">
+        <h1>Smart Helpdesk</h1>
+      </Link>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
     </nav>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,28 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
       <h2>Dashboard</h2>
+      <p> This is a content writing for my smart help desk application</p>
+
+      <div className="flex justify-center my-8">
+        <Link href="/tickets">
+          <button className="btn-primaty">View Tickets</button>
+        </Link>
+      </div>
+
+      <h2>Company Updates</h2>
+
+      <div className="card">
+        <h3>New menber of the web dev team...</h3>
+        <p> this is another content writing</p>
+      </div>
+      <div className="card">
+        <h3>New website live!</h3>
+        <p>another content writing again</p>
+      </div>
     </main>
   )
 }
