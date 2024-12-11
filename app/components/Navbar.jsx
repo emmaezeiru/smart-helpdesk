7871ddbf064671d093c5b34cb5b53d91e3724b0b
emma ezeiru <ezeiruemma5t@gmail.com>
2024-12-11 11:43:00 +0100

    update: changed navbar logo

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Logo from './smart-logo.jpg'
+import Logo from './smarthelp desk.jpg'
 
 export default function Navbar() {
   return (
